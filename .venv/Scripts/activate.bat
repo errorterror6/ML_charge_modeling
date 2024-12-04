b603ad32697fd995c083351c6ed4c83f9269f396
Raymond Li ud790 <raymondli1212@gmail.com>
2024-12-04 21:49:53 +1100

    gitignore update

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\z5183876\Documents\GitHub\ML_charge_modeling\.venv
+set VIRTUAL_ENV=C:\vscode\python\TOR-2024\ML_charge_modeling\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
